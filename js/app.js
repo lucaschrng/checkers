efa287a32333fd31e1639022cd21bb1f9dad4f74
Lucas Charoing <charoing.lucas@gmail.com>
2022-11-10 03:26:52 +0100

    Add moving pieces functionality

--- js/app.js
+++ js/app.js
@@ -24,19 +24,19 @@ for (let i = 0; i < 10; i++) {
 //variables initialisation
 
 let tiles = document.querySelectorAll('.tile');
-let lastClick;
 let whiteTurn = true;
-let color;
-let moveLevel;
-let enemyPosition;
-let emptyTiles;
 let piece;
+let selecetd;
+let previewPiece;
+let previewMoves = [];
 
 let board = [
     // 0: unplayable tile
     // 1: white piece
     // 2: dark piece
     // 3: empty tile
+    // 4: white preview
+    // 5: dark preview
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -54,29 +54,31 @@ let board = [
 class Piece {
     constructor(index) {
         this.index = index;
-        this.column = getColumn(index);
-        this.row = getRow(index);
-        this.color = getColor(index);
+        this.column = index % 10;
+        this.row = Math.floor(index / 10);
+        this.color = board[this.row][this.column];
         this.direction = this.color === 1 ? 1:-1;
-        this.possibleMoves = [];
+        this.possibleMoves = this.getPossibleMoves();
     }
 
     getPossibleMoves() {
 
+        let moves = []
+
         if (this.color === 1 || this.color === 2) {
     
             if (this.row + this.direction >= 0 && this.row + this.direction <= 10) {
-                if (board[this.row + this.direction][this.column - 1] === 3) this.possibleMoves.push([(this.row + this.direction) * 10 + this.column - 1]);
-                if (board[this.row + this.direction][this.column + 1] === 3) this.possibleMoves.push([(this.row + this.direction) * 10 + this.column + 1]);
+                if (board[this.row + this.direction][this.column - 1] === 3) moves.push([(this.row + this.direction) * 10 + this.column - 1]);
+                if (board[this.row + this.direction][this.column + 1] === 3) moves.push([(this.row + this.direction) * 10 + this.column + 1]);
             }
 
             this.getPossibleCaptures(this.row, this.column, this.color, this.direction, []).forEach(capture => {
-                this.possibleMoves.push(capture);
+                moves.push(capture);
             })
             
         }
 
-        return this.possibleMoves;
+        return moves;
     }
 
     getPossibleCaptures(row, column, color, direction, path) {
@@ -120,13 +122,65 @@ class Piece {
 
 tiles.forEach((tile, index) => {
     tile.addEventListener('click', () => {
-        lastClick = index;
-        piece = new Piece(index);
-        console.log(piece.getPossibleMoves());
+
+        console.log(board);
+
+        if (previewMoves.includes(index)) {
+            play(piece.index, index);
+        } else {
+            piece = new Piece(index);
+
+            if ((piece.color === 1 && whiteTurn) || (piece.color === 2 && !whiteTurn)) {
+                preview();
+            }
+        }
     })
 })
 
 updateBoard();
+
+function preview() {
+
+    previewMoves = [];
+
+    updateBoard();
+
+    piece.possibleMoves.forEach(moves => {
+        moves.forEach(move => {
+
+            if (!previewMoves.includes(move)) {
+                previewMoves.push(move);
+            }
+        })
+    })
+
+    previewMoves.forEach(move => {
+        previewPiece = new Piece(move);
+        board[previewPiece.row][previewPiece.column] = whiteTurn ? 4:5;
+    })
+
+    updateBoard();
+
+    previewMoves.forEach(move => {
+        previewPiece = new Piece(move);
+        board[previewPiece.row][previewPiece.column] = 3;
+    })
+}
+
+function play(pointA, pointB) {
+
+    departurePiece = new Piece(pointA);
+    destinationPiece = new Piece(pointB);
+
+    board[departurePiece.row][departurePiece.column] = 3;
+    board[destinationPiece.row][destinationPiece.column] = whiteTurn ? 1:2;
+
+    previewMoves = [];
+
+    updateBoard();
+
+    whiteTurn = !whiteTurn;
+}
 
 function updateBoard() {
     for (let i = 0; i < 10; i++) {
@@ -136,30 +190,38 @@ function updateBoard() {
 
             if (board[i][j] === 1) {
                 document.querySelector(selector).classList.add('light');
+                document.querySelector(selector).classList.remove('brown');
+                document.querySelector(selector).classList.remove('preview-light');
+                document.querySelector(selector).classList.remove('preview-brown');
             }
 
             else if (board[i][j] === 2) {
+                document.querySelector(selector).classList.remove('light');
                 document.querySelector(selector).classList.add('brown');
+                document.querySelector(selector).classList.remove('preview-light');
+                document.querySelector(selector).classList.remove('preview-brown');
+            }
+
+            else if (board[i][j] === 4) {
+                document.querySelector(selector).classList.remove('light');
+                document.querySelector(selector).classList.remove('brown');
+                document.querySelector(selector).classList.add('preview-light');
+                document.querySelector(selector).classList.remove('preview-brown');
+            }
+
+            else if (board[i][j] === 5) {
+                document.querySelector(selector).classList.remove('light');
+                document.querySelector(selector).classList.remove('brown');
+                document.querySelector(selector).classList.remove('preview-light');
+                document.querySelector(selector).classList.add('preview-brown');
             }
 
             else {
                 document.querySelector(selector).classList.remove('light');
                 document.querySelector(selector).classList.remove('brown');
+                document.querySelector(selector).classList.remove('preview-light');
+                document.querySelector(selector).classList.remove('preview-brown');
             }
         }
     }
-}
-
-function getColumn(index) {
-    return index % 10;
-}
-
-function getRow(index) {
-    return Math.floor(index / 10);
-}
-
-function getColor(index) {
-    column = getColumn(index);
-    row = getRow(index);
-    return board[row][column];
 }
